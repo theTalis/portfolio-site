b5a42017c320e1f2d1f36ee601f5ff90697ec02f
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2022-09-12 11:24:11 -0300

    ajuste

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tec-talis/code/projeto_pessoal/portfolio/venv"
+set -gx VIRTUAL_ENV "/Users/tec-talis/code/projeto_pessoal/portfolio-site/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
