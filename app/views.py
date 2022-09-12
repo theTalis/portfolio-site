2d884af3de2d06d9eb46a47046ff2a212f0d2687
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2022-09-11 21:37:30 -0300

    development

--- app/views.py
+++ app/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    data = {
+        "projects": [
+            {
+                "image": "/static/img/aplicativo.png",
+                "description": "Crediario digital",
+                "link": "https://crediario.digital",
+                "text": "projecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdpaokwd pawokdpaowkdapowkdp aokwd paokwd paokwd poawk"
+            },
+            {
+                "image": "/static/img/banking.png",
+                "description": "T2 Banking",
+                "link": "https://app.t2bank.com.br",
+                "text": "projecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdpaokwd pawokdpaowkdapowkdp aokwd paokwd paokwd poawk"
+            },
+            {
+                "image": "/static/img/emprestimo.png",
+                "description": "Credito com vc",
+                "link": "https://credito.com.vc",
+                "text": "projecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdprojecto coisado bastante trabalho coisa e tal e vai e tome e adwojdpoajwdoajwdoijawdojawd apowkdpaokwd pawokdpaowkdapowkdp aokwd paokwd paokwd poawk"
+            }
+        ]
+    }
+    return render(request, 'home.html', data)
