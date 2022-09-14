ae319315c2095215d8ee1a6d8e4d4f3901d727a5
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2022-09-13 22:46:13 -0300

    ajuste

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cfvo9vv8(%7fidj&wkbzc()f8ggd!%5+d@balgsw-l*5m0_3#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['portfolio-taliscipriani.herokuapp.com', 'localhost']
 
